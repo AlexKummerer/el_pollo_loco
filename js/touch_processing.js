d00e77047587b04a0de353d4d992e1acd5b69185
Kummey <76731996+Kummey@users.noreply.github.com>
2021-02-12 13:41:15 +0100

    touch event

--- js/touch_processing.js
+++ js/touch_processing.js
@@ -121,18 +121,3 @@ function detectDoubleTap() {
         })
     );
 }
-
-// function toggleFullScreen() {
-//     let doc = window.document;
-//     let docEl = doc.documentElement;
-
-//     let requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-//     let cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-//     if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-//       requestFullScreen.call(docEl);
-//     }
-//     else {
-//       cancelFullScreen.call(doc);
-//     }
-//   }
